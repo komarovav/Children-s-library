be220cac4f696b0208052162633d9fb1279b756b
komarovav <komarovav022@gmail.com>
2025-12-15 21:12:15 +0300

    db 1.12.25.py

--- Коммиты/db 1.12.25.py
+++ Коммиты/db 1.12.25.py
@@ -2,10 +2,11 @@ import pymysql
 
 def get_connection():
     return (pymysql.connect
-            ( host="5.183.188.132",
-              port=3306,
-              user="2025_mysql__usr7",
-              password="7LsDRBDX7z1v3s9S",
-              database="2025_mysql_ver",
+            ( host="host",
+              port=port,
+              user="user",
+              password="password",
+              database="database",
               cursorclass=pymysql.cursors.DictCursor,
+
               autocommit=True ))
